b14135df9171ac05746a238f931a93ad36992ccb
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2024-01-23 04:57:24 +0600

    setup house-hunter-client-side

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Home from "../Pages/Home/Home";
+
+
+const Layout = () => {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/Router/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default router;
